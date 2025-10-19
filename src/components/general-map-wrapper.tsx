d1fc8b31a275fca641a65f3b5210d9e0339daccf
novoa97 <novoa199@gmail.com>
2025-10-19 16:15:13 +0200

    feat: add search input in the map page

--- src/components/general-map-wrapper.tsx
+++ src/components/general-map-wrapper.tsx
@@ -8,13 +8,15 @@ const GeneralMap = dynamic(() => import("./general-map"), { ssr: false });
 export default function GeneralMapWrapper({
   clients,
   className,
+  center,
 }: {
   clients: ClientWithType[];
+  center?: [number, number];
   className?: string;
 }) {
   return (
     <div className={className}>
-      <GeneralMap clients={clients} />
+      <GeneralMap clients={clients} center={center} />
     </div>
   );
 }
